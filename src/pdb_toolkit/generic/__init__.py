b071b73b835fb30ec39057064260648347105d49
raoufkeskes <raoufkeskes@gmail.com>
2022-09-13 18:29:35 +0200

    align and concat  added and tested succesfully + update readme

--- src/pdb_toolkit/generic/__init__.py
+++ src/pdb_toolkit/generic/__init__.py
@@ -1,3 +1,4 @@
 from .extract_chains_from_pdb import extract_chains_from_pdb
 from .download_pdb import download_pdb
+from .concat_pdbs import concat_pdbs
 from .align import align
---
+++ src/pdb_toolkit/generic/concat_pdbs.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 13/09/2022 15:28
+# @Author  : Raouf KESKES
+# @Email   : raouf.keskes@mabsilico.com
+# @File    : concat_pdbs.py
+
+# generic
+import glob
+import os
+
+
+def concat_pdbs(in_pdb_files, out_pdb_file):
+    list_pdb_files = ""
+    # if it is a directory of files to concatenate
+    try:
+        list_pdb_files = glob.glob(in_pdb_files + "/*.pdb")
+    except:
+        # else it is a list of files
+        list_pdb_files = in_pdb_files
+
+    # print("cat {} >> {}".format(" ".join(list_pdb_files), out_pdb_file))
+    os.system("cat {} >> {}".format(" ".join(list_pdb_files), out_pdb_file))
