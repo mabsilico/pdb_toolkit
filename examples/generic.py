742bb8e9e3fd0f0fe5f6b8bd602b3cba54ddb969
raoufkeskes <raoufkeskes@gmail.com>
2022-12-20 15:03:57 +0100

    - add protonate + sort options for extract_chains_from_pdb

---
+++ examples/generic.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 24/10/2022 16:00
+# @Author  : Raouf KESKES
+# @Email   : raouf.keskes@mabsilico.com
+# @File    : generic.py
+
+
+from pdb_toolkit.generic import *
+
+download_pdb(pdb_id="7nd4", output_dir="./Downloads/", overwrite=True)
