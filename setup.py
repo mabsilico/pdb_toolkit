59fad3f86f39df1ed53ff15db1c39c5a4e91d3f4
raoufkeskes <raoufkeskes@gmail.com>
2023-08-23 16:20:02 +0200

    update the setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ from setuptools import setup, find_packages
 
 
 # Read the contents of your README file
-with open('README.md', encoding='utf-8') as f:
-    description = f.read()
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 setup(
     name='pdb-toolkit',
-    version='0.3.4',
+    version='0.3.5',
     packages=find_packages(),
     url='https://github.com/raoufkeskes/pdb_toolkit',
     license='MIT Licence',
     author='raouf-ks',
     author_email='raouf.keskes@mabsilico.com',
-    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     python_requires='>=3.6',
     install_requires=['wget'],
     classifiers=[
@@ -22,4 +23,3 @@ setup(
             "Operating System :: OS Independent"
         ],
 )
-
